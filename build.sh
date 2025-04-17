a0f9fb954231184e3c59cc25d1e21fc2199db121
Alex Davis <xandaron@gmail.com>
2025-04-17 23:53:03 +0100

    fixed linux build script

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
 BASEDIR=$(dirname "$0")
-gcc -c $BASEDIR/deps/ufbx.c -o $BASEDIR/deps/ufbx.o
+clang -c $BASEDIR/deps/ufbx.c -o $BASEDIR/deps/ufbx.o -O3 -D"UFBX_REAL_IS_FLOAT"="1"
 ar r $BASEDIR/ufbx/ufbx.a $BASEDIR/deps/ufbx.o
 rm $BASEDIR/deps/ufbx.o
