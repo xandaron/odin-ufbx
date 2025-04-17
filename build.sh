591b37bd54a6077dfedf3af16f812dde5791178e
Alex Davis <xandaron@gmail.com>
2025-04-17 21:19:28 +0100

    Update build process

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
 BASEDIR=$(dirname "$0")
-gcc -c $BASEDIR/deps/ufbx.c
+gcc -c $BASEDIR/deps/ufbx.c -o $BASEDIR/deps/ufbx.o
 ar r $BASEDIR/ufbx/ufbx.a $BASEDIR/deps/ufbx.o
 rm $BASEDIR/deps/ufbx.o
